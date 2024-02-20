20cecbc1e4c73f47ce43227342017402e6c681da
Adonis Aleman <adonis.aleman@unadeca.net>
2024-02-20 11:10:13 -0600

    Header y cambio de nombre

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
+import Header from "./componentes/Header";
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <Header />
+      </div>
       <div>
         <h1>My First React App</h1>
         <p>
