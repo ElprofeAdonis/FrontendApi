b988525c41d49f85698a6bd311ba110b06412ff3
Adonis Aleman <adonis.aleman@unadeca.net>
2024-02-26 23:11:14 -0600

    Listo el post

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import Header from "./componentes/Header";
 import React, { useState, useEffect } from "react";
 import { get } from "./util/apiClient";
+import { post } from "./util/apiClient";
 
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
 const App = () => {
-  const [infoMovies, setMovies] = useState(null);
+  const [infoMovies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [newMovie, setNewMovie] = useState({
+    nombre: "",
+    director: "",
+    clasificacion: "",
+  });
 
   useEffect(() => {
     const fetchdata = async () => {
@@ -27,6 +33,16 @@ const App = () => {
   }, []);
   console.log(infoMovies);
 
+  function handleAddMovie() {
+    post("/peliculas", newMovie)
+      .then((response) => {
+        const updateMovies = [...infoMovies, response];
+        setMovies(updateMovies);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies", error);
+      });
+  }
   if (isLoading) {
     return <div>Loading movies...</div>;
   }
@@ -73,6 +89,33 @@ const App = () => {
         </div>
         <div style={{ border: "1px solid red", width: "30%" }}>
           Hola Soy Adonis
+          <div>
+            <input
+              type="text"
+              placeholder="Nombre de la película"
+              value={newMovie.nombre}
+              onChange={(event) =>
+                setNewMovie({ ...newMovie, nombre: event.target.value })
+              }
+            />
+            <input
+              type="text"
+              placeholder="Director"
+              value={newMovie.director}
+              onChange={(event) =>
+                setNewMovie({ ...newMovie, director: event.target.value })
+              }
+            />
+            <input
+              type="text"
+              placeholder="Clasificación"
+              value={newMovie.clasificacion}
+              onChange={(event) =>
+                setNewMovie({ ...newMovie, clasificacion: event.target.value })
+              }
+            />
+            <button onClick={handleAddMovie}>Agregar película</button>
+          </div>
         </div>
       </div>
     </div>
